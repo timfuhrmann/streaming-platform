4e376cf031752b91f6f7dc77ac1a6344af49d533
timfuhrmann <tf@becklyn.com>
2021-11-04 21:45:35 +0100

    Add breakpoints

--- app/layout/organism/BlockBasicSlider.tsx
+++ app/layout/organism/BlockBasicSlider.tsx
@@ -41,7 +41,15 @@ export const BlockBasicSlider: React.FC<BlockBasicSliderProps> = ({ title, shows
     const containerRef = useRef<HTMLDivElement | null>(null);
 
     const { mounted } = useSlider(containerRef, {
-        slidesPerView: 6,
+        slidesPerView: 2,
+        breakpoints: {
+            "(min-width: 768px)": {
+                slidesPerView: 4,
+            },
+            "(min-width: 1340px)": {
+                slidesPerView: 6,
+            },
+        },
     });
 
     return (
